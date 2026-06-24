1c367338c9e5539f8485165fe3f26b8159d0d827
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-06-24 14:03:39 +0500

    auth controller and route updated

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { googleAuth } from "../controllers/auth.controller.js";
+import { googleAuth, logOut } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/google",googleAuth)
-
+authRouter.get("/logout",logOut)
 
 export default authRouter
