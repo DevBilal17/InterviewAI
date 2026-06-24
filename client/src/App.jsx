0fd6d09de096a35de3382ed7448389a63e857a87
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-06-24 18:41:28 +0500

    Current user api called in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import axios from 'axios'
+import { SERVER_URL } from './utils/constants'
 
 function App() {
+  useEffect(()=>{
+    const getUser = async () => {
+      try {
+        const res = await axios.get(`${SERVER_URL}/api/user/current-user`,{withCredentials:true});
+        console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUser()
+  },[])
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
