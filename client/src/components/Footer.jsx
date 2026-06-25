e5c164a00e1a0dc6e2c4024a9cb80368954256a6
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-06-25 11:31:31 +0500

    Footer Updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BsRobot } from 'react-icons/bs'
 
 function Footer() {
   return (
-    <div className='bg-[#f3f3f3] flex justify-center px-4 pb-10 py-4 pt-10'>
+    <div className='bg-[#f3f3f3] flex justify-center px-4 pb-10 py-4'>
       <div className='w-full max-w-6xl bg-white rounded-3xl shadow-sm border border-gray-200 py-8 px-3 items-center'>
         <div className='flex justify-center items-center gap-3 mb-3'>
             <div className='bg-black text-white p-2 rounded-lg'>
@@ -11,7 +11,7 @@ function Footer() {
             </div>
             <h2 className='font-semibold'>InterviewAI</h2>
         </div>
-        <p className='text-gray-500 text-sm max-w-xl mx-auto'>
+        <p className='text-gray-500 text-sm max-w-xl  mx-auto text-center'>
             AI-powered interview preparation platform designed to improve communication skills, technical depth and professional confidence.
         </p>
       </div>
