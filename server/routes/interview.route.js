0c880ae2b24b5d228bf5048ff687942484db09fa
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-06-25 17:16:24 +0500

    Interview Route done and implemented

---
+++ server/routes/interview.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { upload } from "../middlewares/multer.js";
+import { analyzeResume } from "../controllers/interview.controller.js";
+import isAuth from "../middlewares/isAuth.js"
+const interviewRouter = express.Router();
+
+
+interviewRouter.post("/resume",isAuth,upload.single("resume"),analyzeResume);
+
+
+export default interviewRouter
