3e5300dc7ca8397f7738e17ba50bd03bd2460361
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-06-27 18:59:31 +0500

    Toggle mic , stop and start mic function created

--- client/src/components/InterviewStep.jsx
+++ client/src/components/InterviewStep.jsx
@@ -182,11 +182,40 @@ function InterviewStep({ interviewData, onFinish }) {
     };
 
     recognitionRef.current = recognition;
-    return () => {
-      recognition.stop();
-      recognitionRef.current = null;
-    };
+    // return () => {
+    //   recognition.stop();
+    //   recognitionRef.current = null;
+    // };
   }, []);
+
+  const startMic = () => {
+    if(recognitionRef.current && !isAIPlaying){
+      try {
+        recognitionRef.current.start()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  const stopMic = () =>{
+     if(recognitionRef.current){
+      recognitionRef.current.stop();
+     }
+  }
+
+  const toggleMic = () => {
+    if(isMicOn){
+      stopMic()
+    }
+    else{
+      startMic()
+    }
+    setIsMicOn(!isMicOn)
+  }
+
+
+
 
   return (
     <div className="min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6">
