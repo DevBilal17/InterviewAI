e74c4248c9279b73178c373c0f9a05a3e5a76703
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-07-05 10:12:16 +0500

    Interview Controller updated

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
-
+import { useEffect } from 'react';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from "axios"
+import { SERVER_URL } from '../utils/constants';
 function History() {
+    const [interviews,setInterviews] = useState([]);
+    const navigate = useNavigate()
+
+  useEffect(()=>{
+    const getMyInterviews = async () => {
+        try {
+            const res = await axios.get(`${SERVER_URL}/api/interview/get-interviews`,{
+                withCredentials : true
+            })
+
+            console.log(res?.data)
+            setInterviews(res?.data?.interviews)
+        } catch (error) {
+            console.log(error)
+        }    
+    }
+    getMyInterviews()
+  },[])
   return (
     <div>
       History
