384ca0d31de363bbc983eae37b0e4b92edc6103c
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-07-07 18:26:07 +0500

    Pricing Page Header done

--- client/src/pages/Pricing.jsx
+++ client/src/pages/Pricing.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
 function Pricing() {
+  const navigate = useNavigate()
   return (
-    <div>
-      Pricing
+    <div className='min-h-screen bg-linear-to-br from-gray-50 to-emerald-50 py-16 px-6'>
+      <div className='max-w-6xl mx-auto mb-14 flex items-start gap-4'>
+        <button onClick={()=>navigate("/")} className='mt-2 p-3 rounded-full bg-white shadow hover:shadow-md transition'>
+          <FaArrowLeft className='text-gray-600' />
+        </button>
+
+        <div className='text-center w-full'>
+          <h1 className='text-4xl font-bold text-gray-800'>
+            Choose Your Plan
+          </h1>
+          <p className='text-gray-500 mt-3 text-lg'>
+            Flexible pricing to match your interview preparation goals.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
